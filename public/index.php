28dabf7b61cb04e1d94d698796e3099efdb02849
Amr sayed <amrsaid@gmail.com>
2026-06-18 16:18:09 +0300

    index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use Illuminate\Foundation\Bootstrap\RegisterFacades;
+use Illuminate\Foundation\Bootstrap\RegisterProviders;
+use Illuminate\Foundation\Bootstrap\BootProviders;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
